bc90d84ffcaa061e050bc57243c38d6018b9d9b1
Stefan Novak <stefannovak96@gmail.com>
2020-03-20 13:33:49 +0000

    arrayOf

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,13 @@
+fun sayHello(greeting:String, itemToGreet:String) = println("$greeting $itemToGreet")
+
 fun main() {
-    println("Hello World")
+    val interestingThings = arrayOf("Kotlin", "Programming", "Comic Books")
+    println(interestingThings.size)
+    println(interestingThings[0])
+    println(interestingThings.get(2))
+
+    //
+    for(interestingThing in interestingThings) {
+        println(interestingThing)
+    }
 }
